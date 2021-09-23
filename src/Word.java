5a928400876f72f565499f5a8140ab3c365cc589
= <=>
2021-09-23 22:06:26 +0700

    first commit

---
+++ src/Word.java
@@ -0,0 +1,46 @@
+public class Word {
+    private String word_target;
+    private String word_explain;
+
+    /**
+     * Constructor.
+     * @param word_target
+     * @param word_explain
+     */
+    Word(String word_target, String word_explain) {
+        this.word_target = word_target;
+        this.word_explain = word_explain;
+    }
+
+    /**
+     * set word_target.
+     * @param word_target
+     */
+    public void setWordTarget(String word_target) {
+        this.word_target = word_target;
+    }
+
+    /**
+     * get word_target.
+     * @return
+     */
+    public String getWordTarget() {
+        return this.word_target;
+    }
+
+    /**
+     * set word_explain.
+     * @param word_explain
+     */
+    public void setWordExplain(String word_explain) {
+        this.word_explain = word_explain;
+    }
+
+    /**
+     * get word_explain.
+     * @return
+     */
+    public String getWordExplain() {
+        return this.word_explain;
+    }
+}

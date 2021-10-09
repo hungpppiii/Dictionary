2cc1020f4498f4f25bb02f97104173417d1337f3
= <=>
2021-10-09 08:39:05 +0700

    update translate

--- src/main/java/dict/Main.java
+++ src/main/java/dict/Main.java
@@ -4,10 +4,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        
+        //test translate
+        Translate trans = new Translate();
+        System.out.println(trans.translate("If I pass a string (either in English or Arabic) as an input"));
+        //
         DictionaryCommandline dictCommandline = new DictionaryCommandline();
         Scanner sc = new Scanner(System.in);
         String value;
-        while(true) {
+        while (true) {
             System.out.println("\t1.vao dictionary");
             System.out.println("\t2.exit");
             System.out.print("nhap lua chon: ");
@@ -15,8 +20,8 @@ public class Main {
             if (value.equals("1")) {
                 dictCommandline.dictionaryBasic();
             } else if (value.equals("2")) {
-                  break;
-            } 
+                break;
+            }
         }
     }
 }
